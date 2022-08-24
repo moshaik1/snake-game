de8c702a9131597c78330d8d609f48975f4712c7
mohsin shaik <mohsinshaik2017@gmail.com>
2022-08-24 06:06:03 -0500

    testing github

---
+++ GameFrame.java
@@ -0,0 +1,8 @@
+/// java swing
+public GameFrame{}
+public static void main(String[] args)
+{
+System.out.println("Hello snake");
+
+}
+}
---
+++ SnakeGame.java
@@ -0,0 +1,8 @@
+/// snake game logic
+public snakeGame{
+public static void main(String[] args)
+{
+System.out.println("Hello human");
+
+}
+}
